f37e25769c85d830076e6a5d5475fab2c98b27e8
TETRA2000 <sagittariusm25@gmail.com>
2016-03-21 22:21:35 +0900

    ripple animation

--- WaveC4/WorkSpace.swift
+++ WaveC4/WorkSpace.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
+
 class WorkSpace: CanvasController {
-
+    
     override func setup() {
-        //Work your magic here.
+        canvas.addTapGestureRecognizer { (locations, center, state) -> () in
+            print(center)
+            self.startRipple(center)
+        }
+        
     }
-
-
+    
+    private func startRipple(position: Point) {
+        let animDuration = 1.6
+        
+        let ripple = Circle(center: position, radius: 24.0)
+        ripple.strokeColor = Color.init(UIColor.clearColor())
+        canvas.add(ripple)
+        
+        let anim = ViewAnimation(duration: animDuration, animations: {
+            ripple.transform.scale(24.0, 24.0)
+            ripple.fillColor = Color.init(UIColor.clearColor())
+        })
+        
+        anim.animate()
+        wait(animDuration) {
+            self.canvas.remove(ripple)
+        }
+    }
 }
 
