b1850d6a2da8717b586bdf7ac0ab004d7f6895e6
TETRA2000 <sagittariusm25@gmail.com>
2016-03-21 19:09:10 +0900

    init repo

---
+++ WaveC4/WorkSpace.swift
@@ -0,0 +1,19 @@
+//
+//  WorkSpace.swift
+//  WaveC4
+//
+//  Created by Takahiko Inayama on 3/21/16.
+//  Copyright © 2016 TETRA2000. All rights reserved.
+//
+
+import UIKit
+
+class WorkSpace: CanvasController {
+
+    override func setup() {
+        //Work your magic here.
+    }
+
+
+}
+
